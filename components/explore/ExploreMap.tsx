5ad6708a0d3d50ccf65413569571776b047a8ffa
Ali Smesseim <smesseim.ali@gmail.com>
2025-12-30 23:10:34 +0100

    Make images fit pin

--- components/explore/ExploreMap.tsx
+++ components/explore/ExploreMap.tsx
@@ -366,6 +366,8 @@ export default function ExploreMap({ className }: ExploreMapProps) {
           width: 100%;
           height: 100%;
           object-fit: cover;
+          min-width: 50px;
+          min-height: 50px;
         }
         
         .cluster-count {
